c9a06153159e073a4f6ed11c2cf4db82be3fdb12
oner <740778222@qq.com>
2019-04-10 10:25:00 +0800

    add info

--- src/com/oner/first/first.java
+++ src/com/oner/first/first.java
@@ -13,7 +13,7 @@ public class first {
     public static void main(String[] args) {
         System.out.println("hello!!!");
         for (int i = 0; i < 5; i++) {
-            
+            System.out.println("11");
         }
 
 
