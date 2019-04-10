edefa2474b0d6a601d4e660b510d4b5020ed1d41
oner <740778222@qq.com>
2019-04-10 09:58:07 +0800

    Initial commit

---
+++ src/com/oner/first/first.java
@@ -0,0 +1,23 @@
+package com.oner.first;
+
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈和___相关的service〉
+ *
+ * @author 作者:oner
+ * @create 修改时间:2019/4/8
+ * @since 1.0.0
+ */
+public class first {
+    public static void main(String[] args) {
+        System.out.println("hello!!!");
+        for (int i = 0; i < 5; i++) {
+            
+        }
+
+
+    }
+
+
+}
